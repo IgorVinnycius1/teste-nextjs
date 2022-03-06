4ef5e1abf66993caa441ffbc251d49643879696e
IgorVinnycius1 <igor.vinnycius1@gmail.com>
2022-03-06 15:33:25 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,6 @@
 import { useState } from 'react';
 
 function Home(){
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    return <div> <h1>Home</h1> </div>
 }
 export default Home
